743dec9a20a1cc6165ca4ea869694737b6026e6c
KittenCN <owdely@vip.163.com>
2023-03-15 08:58:55 +0800

    modify code for generic data

--- predict.py
+++ predict.py
@@ -15,102 +15,19 @@ from torch.utils.data import Dataset,DataLoader
 import math
 import numpy as np
 import os
+from tqdm import tqdm
 
-#数据清洗：丢弃行，或用上一行的值填充
-def data_wash(dataset,keepTime=False):
-    if keepTime:
-        dataset.fillna(axis=1,method='ffill')
-    else:
-        dataset.dropna()
-    return dataset
+#读取数据切割数据集并保存
+TRAIN_WEIGHT=0.9
+SEQ_LEN=99
+LEARNING_RATE=0.00001
+BATCH_SIZE=4
+EPOCH=10
+SAVE_NUM=100
 
-def import_csv(stock_code):
-    #time设为index的同时是否保留时间列
-    df = pd.read_csv('stock_daily/'+stock_code + '.csv')
-    #清洗数据
-    df=data_wash(df,keepTime=False)
-    df.rename(
-            columns={
-            'trade_date': 'Date', 'open': 'Open', 
-            'high': 'High', 'low': 'Low', 
-            'close': 'Close', 'vol': 'Volume'}, 
-            inplace=True)
-    df['Date'] = pd.to_datetime(df['Date'],format='%Y%m%d')    
-    df.set_index(df['Date'], inplace=True)
-    return df
+mean_list=[]
+std_list=[]
 
-def draw_Kline(df,period,symbol):
-
-    # 设置基本参数
-    # type:#绘制图形的类型，有candle, renko, ohlc, line等
-    # 此处选择candle,即K线图
-    # mav(moving average):均线类型,此处设置7,30,60日线
-    # volume:布尔类型，设置是否显示成交量，默认False
-    # title:设置标题
-    # y_label:设置纵轴主标题
-    # y_label_lower:设置成交量图一栏的标题
-    # figratio:设置图形纵横比
-    # figscale:设置图形尺寸(数值越大图像质量越高)
-    kwargs = dict(
-        type='candle', 
-        mav=(7, 30, 60), 
-        volume=True, 
-        title='\nA_stock %s candle_line' % (symbol),    
-        ylabel='OHLC Candles', 
-        ylabel_lower='Shares\nTraded Volume', 
-        figratio=(15, 10), 
-        figscale=2)
-
-    # 设置marketcolors
-    # up:设置K线线柱颜色，up意为收盘价大于等于开盘价
-    # down:与up相反，这样设置与国内K线颜色标准相符
-    # edge:K线线柱边缘颜色(i代表继承自up和down的颜色)，下同。详见官方文档)
-    # wick:灯芯(上下影线)颜色
-    # volume:成交量直方图的颜色
-    # inherit:是否继承，选填
-    mc = mpf.make_marketcolors(
-        up='red', 
-        down='green', 
-        edge='i', 
-        wick='i', 
-        volume='in', 
-        inherit=True)
-
-    # 设置图形风格
-    # gridaxis:设置网格线位置
-    # gridstyle:设置网格线线型
-    # y_on_right:设置y轴位置是否在右
-    s = mpf.make_mpf_style(
-        gridaxis='both', 
-        gridstyle='-.', 
-        y_on_right=False, 
-        marketcolors=mc)
-
-    # 设置均线颜色，配色表可见下图
-    # 建议设置较深的颜色且与红色、绿色形成对比
-    # 此处设置七条均线的颜色，也可应用默认设置
-    mpl.rcParams['axes.prop_cycle'] = cycler(
-        color=['dodgerblue', 'deeppink', 
-        'navy', 'teal', 'maroon', 'darkorange', 
-        'indigo'])
-    
-    # 设置线宽
-    mpl.rcParams['lines.linewidth'] = .5
-
-    # 图形绘制
-    # show_nontrading:是否显示非交易日，默认False
-    # savefig:导出图片，填写文件名及后缀
-    mpf.plot(df, 
-        **kwargs, 
-        style=s, 
-        show_nontrading=False,)
-    mpf.plot(df, 
-        **kwargs, 
-        style=s, 
-        show_nontrading=False,
-        savefig='A_stock-%s %s_candle_line'
-        %(symbol, period) + '.jpg')
-    plt.show()
 #完成数据集类
 class Stock_Data(Dataset):
 
@@ -213,79 +130,9 @@ class TransAm(nn.Module):
         output=self.linear1(output)
         return output
 
-def train(epoch):
-    model.train()
-    global loss_list
-    global iteration
-    dataloader=DataLoader(dataset=stock_train,batch_size=BATCH_SIZE,shuffle=False,drop_last=True)
-    for i,(data,label) in enumerate(dataloader):
-        iteration=iteration+1
-        data,label = data.to(device),label.to(device)
-        optimizer.zero_grad()
-        output=model.forward(data)
-        loss=criterion(output,label)
-        loss.backward()        
-        optimizer.step()
-        if i%20==0:
-            loss_list.append(loss.item())
-            print("epoch=",epoch,"iteration=",iteration,"loss=",loss.item())
-        if epoch%EPOCH==0:
-            torch.save(model.state_dict(),save_path+"_Model.pkl")
-            torch.save(optimizer.state_dict(),save_path+"_Optimizer.pkl")
-
-def test():
-    model.eval()
-    global accuracy_list
-    global predict_list
-    dataloader=DataLoader(dataset=stock_test,batch_size=BATCH_SIZE,shuffle=False,drop_last=True)
-    for i,(data,label) in enumerate(dataloader):
-        with torch.no_grad():            
-            data,label=data.to(device),label.to(device)
-            optimizer.zero_grad()
-            predict=model.forward(data)
-            predict_list.append(predict)
-            loss=criterion(predict,label)
-            accuracy_fn=nn.MSELoss()
-            accuracy=accuracy_fn(predict,label)
-            accuracy_list.append(accuracy.item())
-    print("test_data MSELoss:(pred-real)/real=",np.mean(accuracy_list))
-
-def loss_curve(loss_list):
-    x=np.linspace(1,len(loss_list),len(loss_list))
-    x=20*x
-    plt.plot(x,np.array(loss_list),label="train_loss")
-    plt.ylabel("MSELoss")
-    plt.xlabel("iteration")
-    plt.savefig("train_loss.png",dpi=3000)
-    plt.show()
-
-def contrast_lines(predict_list):
-    real_list=[]
-    prediction_list=[]
-    dataloader=DataLoader(dataset=stock_test,batch_size=4,shuffle=False,drop_last=True)
-    for i,(data,label) in enumerate(dataloader):
-        for idx in range(BATCH_SIZE):
-            real_list.append(np.array(label[idx]*std_list[0]+mean_list[0]))
-    for item in predict_list:
-        item=item.to("cpu")
-        for idx in range(BATCH_SIZE):
-            prediction_list.append(np.array((item[idx]*std_list[0]+mean_list[0])))
-    x=np.linspace(1,len(real_list),len(real_list))
-    plt.plot(x,np.array(real_list),label="real")
-    plt.plot(x,np.array(prediction_list),label="prediction")
-    plt.legend()
-    plt.savefig(cnname+"_Pre.png",dpi=3000)
-    plt.show()
-
 stock_train=Stock_Data(train=True)
 stock_test=Stock_Data(train=False)
 
-#读取数据切割数据集并保存
-TRAIN_WEIGHT=0.9
-SEQ_LEN=99
-LEARNING_RATE=0.00001
-BATCH_SIZE=4
-EPOCH=10
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #train_size=int(TRAIN_WEIGHT*(data.shape[0]))
 train_path="stock_daily/stock_train.csv"
@@ -295,12 +142,9 @@ test_path="stock_daily/stock_test.csv"
 #Train_data.to_csv(train_path,sep=',',index=False,header=False)
 #Test_data.to_csv(test_path,sep=',',index=False,header=False)
 
-mean_list=[]
-std_list=[]
-
 # lstm_path="./model_lstm/epoch_"
 # transformer_path="./model_transformer/epoch_"
-symbol = '000001.SZ'
+symbol = '600975.SH'
 cnname = ""
 for item in symbol.split("."):
     cnname += item
@@ -319,26 +163,198 @@ save_path=lstm_path
 model=model.to(device)
 criterion=nn.MSELoss()
 
+test_loss = 0.00
+
 if os.path.exists("./"+cnname+"/LSTM_Model.pkl"):
     model.load_state_dict(torch.load("./"+cnname+"/LSTM_Model.pkl"))
 optimizer=optim.Adam(model.parameters(),lr=LEARNING_RATE)
 if os.path.exists("./"+cnname+"/LSTM_Optimizer.pkl"):
     optimizer.load_state_dict(torch.load("./"+cnname+"/LSTM_Optimizer.pkl"))
 
+
+#数据清洗：丢弃行，或用上一行的值填充
+def data_wash(dataset,keepTime=False):
+    if keepTime:
+        dataset.fillna(axis=1,method='ffill')
+    else:
+        dataset.dropna()
+    return dataset
+
+def import_csv(stock_code):
+    #time设为index的同时是否保留时间列
+    df = pd.read_csv('stock_daily/'+stock_code + '.csv')
+    #清洗数据
+    df=data_wash(df,keepTime=False)
+    df.rename(
+            columns={
+            'trade_date': 'Date', 'open': 'Open', 
+            'high': 'High', 'low': 'Low', 
+            'close': 'Close', 'vol': 'Volume'}, 
+            inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'],format='%Y%m%d')    
+    df.set_index(df['Date'], inplace=True)
+    return df
+
+def draw_Kline(df,period,symbol):
+
+    # 设置基本参数
+    # type:#绘制图形的类型，有candle, renko, ohlc, line等
+    # 此处选择candle,即K线图
+    # mav(moving average):均线类型,此处设置7,30,60日线
+    # volume:布尔类型，设置是否显示成交量，默认False
+    # title:设置标题
+    # y_label:设置纵轴主标题
+    # y_label_lower:设置成交量图一栏的标题
+    # figratio:设置图形纵横比
+    # figscale:设置图形尺寸(数值越大图像质量越高)
+    kwargs = dict(
+        type='candle', 
+        mav=(7, 30, 60), 
+        volume=True, 
+        title='\nA_stock %s candle_line' % (symbol),    
+        ylabel='OHLC Candles', 
+        ylabel_lower='Shares\nTraded Volume', 
+        figratio=(15, 10), 
+        figscale=2)
+
+    # 设置marketcolors
+    # up:设置K线线柱颜色，up意为收盘价大于等于开盘价
+    # down:与up相反，这样设置与国内K线颜色标准相符
+    # edge:K线线柱边缘颜色(i代表继承自up和down的颜色)，下同。详见官方文档)
+    # wick:灯芯(上下影线)颜色
+    # volume:成交量直方图的颜色
+    # inherit:是否继承，选填
+    mc = mpf.make_marketcolors(
+        up='red', 
+        down='green', 
+        edge='i', 
+        wick='i', 
+        volume='in', 
+        inherit=True)
+
+    # 设置图形风格
+    # gridaxis:设置网格线位置
+    # gridstyle:设置网格线线型
+    # y_on_right:设置y轴位置是否在右
+    s = mpf.make_mpf_style(
+        gridaxis='both', 
+        gridstyle='-.', 
+        y_on_right=False, 
+        marketcolors=mc)
+
+    # 设置均线颜色，配色表可见下图
+    # 建议设置较深的颜色且与红色、绿色形成对比
+    # 此处设置七条均线的颜色，也可应用默认设置
+    mpl.rcParams['axes.prop_cycle'] = cycler(
+        color=['dodgerblue', 'deeppink', 
+        'navy', 'teal', 'maroon', 'darkorange', 
+        'indigo'])
+    
+    # 设置线宽
+    mpl.rcParams['lines.linewidth'] = .5
+
+    # 图形绘制
+    # show_nontrading:是否显示非交易日，默认False
+    # savefig:导出图片，填写文件名及后缀
+    mpf.plot(df, 
+        **kwargs, 
+        style=s, 
+        show_nontrading=False,)
+    mpf.plot(df, 
+        **kwargs, 
+        style=s, 
+        show_nontrading=False,
+        savefig='A_stock-%s %s_candle_line'
+        %(symbol, period) + '.jpg')
+    plt.show()
+
+def train(epoch):
+    model.train()
+    global loss_list
+    global iteration
+    dataloader=DataLoader(dataset=stock_train,batch_size=BATCH_SIZE,shuffle=False,drop_last=True)
+    for i,(data,label) in enumerate(dataloader):
+        iteration=iteration+1
+        data,label = data.to(device),label.to(device)
+        optimizer.zero_grad()
+        output=model.forward(data)
+        loss=criterion(output,label)
+        loss.backward()        
+        optimizer.step()
+        if i%20==0:
+            loss_list.append(loss.item())
+            # print("epoch=",epoch,"iteration=",iteration,"loss=",loss.item())
+            pbar.set_description("ep=%d,iter=%d,lo=%.4f,tl=%.4f"%(epoch,iteration,loss.item(),test_loss))
+            pbar.update(20)
+        if iteration%SAVE_NUM==0:
+            torch.save(model.state_dict(),save_path+"_Model.pkl")
+            torch.save(optimizer.state_dict(),save_path+"_Optimizer.pkl")
+
+def test():
+    model.eval()
+    global accuracy_list, predict_list, test_loss
+    dataloader=DataLoader(dataset=stock_test,batch_size=BATCH_SIZE,shuffle=False,drop_last=True)
+    for i,(data,label) in enumerate(dataloader):
+        with torch.no_grad():            
+            data,label=data.to(device),label.to(device)
+            optimizer.zero_grad()
+            predict=model.forward(data)
+            predict_list.append(predict)
+            loss=criterion(predict,label)
+            accuracy_fn=nn.MSELoss()
+            accuracy=accuracy_fn(predict,label)
+            accuracy_list.append(accuracy.item())
+    # print("test_data MSELoss:(pred-real)/real=",np.mean(accuracy_list))
+    test_loss = np.mean(accuracy_list)
+
+def loss_curve(loss_list):
+    x=np.linspace(1,len(loss_list),len(loss_list))
+    x=20*x
+    plt.plot(x,np.array(loss_list),label="train_loss")
+    plt.ylabel("MSELoss")
+    plt.xlabel("iteration")
+    plt.savefig("train_loss.png",dpi=3000)
+    # plt.show()
+
+def contrast_lines(predict_list):
+    real_list=[]
+    prediction_list=[]
+    dataloader=DataLoader(dataset=stock_test,batch_size=4,shuffle=False,drop_last=True)
+    for i,(data,label) in enumerate(dataloader):
+        for idx in range(BATCH_SIZE):
+            real_list.append(np.array(label[idx]*std_list[0]+mean_list[0]))
+    for item in predict_list:
+        item=item.to("cpu")
+        for idx in range(BATCH_SIZE):
+            prediction_list.append(np.array((item[idx]*std_list[0]+mean_list[0])))
+    x=np.linspace(1,len(real_list),len(real_list))
+    plt.plot(x,np.array(real_list),label="real")
+    plt.plot(x,np.array(prediction_list),label="prediction")
+    plt.legend()
+    plt.savefig(cnname+"_Pre.png",dpi=3000)
+    # plt.show()
+
 if __name__=="__main__":
     period = 100
     data = import_csv(symbol)
     df_draw=data[-period:]
-    draw_Kline(df_draw,period,symbol)
+    # draw_Kline(df_draw,period,symbol)
     data.drop(['ts_code','Date'],axis=1,inplace = True)    
+    train_size=int(TRAIN_WEIGHT*(data.shape[0]))
+    Train_data=data[:train_size+SEQ_LEN]
+    Test_data=data[train_size-SEQ_LEN:]
+    Train_data.to_csv(train_path,sep=',',index=False,header=False)
+    Test_data.to_csv(test_path,sep=',',index=False,header=False)
     iteration=0
     loss_list=[]
     #开始训练神经网络
+    pbar = tqdm(total=len(stock_train)//BATCH_SIZE*EPOCH)
     for epoch in range(1,EPOCH+1):
         predict_list=[]
         accuracy_list=[]
         train(epoch)
         test()
+    pbar.close()
     #绘制损失函数下降曲线    
     loss_curve(loss_list)
     #绘制测试集pred-real对比曲线
